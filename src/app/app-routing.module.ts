b5fc255b5762b2198d684cf4779f14d7f7b2b3bf
Victor <VictorCarmona9035@hotmail.com>
2022-07-30 20:04:33 +0200

    2-Rutas hijasy LazyLoad - AuthRoutes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+//import { CommonModule } from '@angular/common';
+
+const routes:Routes=[
+  {
+    path:'auth',
+    loadChildren:()=>import('./auth/auth.module').then(m=>m.AuthModule)
+  },
+  {
+    path:'404',
+    component:ErrorPageComponent
+  },
+  {
+    path:'**',
+    //component:ErrorPageComponent
+    redirectTo:'404'
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    //CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
